6ab5165337cfa91bbe563eed2f0c4faccc96110b
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-06-07 11:38:52 +0300

    Add class based view for creating new posts

--- gram/views.py
+++ gram/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
-
 # Create your views here.
-
-
-
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'gram/home.html', context)
+    
+class PostListView(ListView):
+    model = Post
+    template_name = 'gram/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
+    
+class PostDetailView(DetailView):
+    model = Post   
+    
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','image',  'content']
 
 def about(request):
     return render(request, 'gram/about.html', {'title':'About'})
